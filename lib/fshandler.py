65d5527ddb71978717ee84df20c7347cd8b837fb
GearFox98 <riveroluismanuel9@gmail.com>
2021-02-25 08:22:45 -0500

    Adding databases and new requirements

--- lib/fshandler.py
+++ lib/fshandler.py
@@ -4,12 +4,50 @@ Created on Fri Jan 22 21:04:15 2021
 FileSystem Handler
 """
 
+import pymongo
+from bson.objectid import ObjectId
 import os.path as ph
 import os
 
 SYS = "../sys/"
 LOG = "../logs/log_"
 IS_EVENT = False
+DB_NAME = "clockworkfox-bot"
+CLIENT = os.environ['MONGO']
+
+cli = pymongo.MongoClient(CLIENT)
+
+def writeEventList(gId, content):
+  collection = "event-content"
+  db = cli[DB_NAME][collection]
+  try:
+    db.insert_one({"_id": gId, "list": content})
+  except Exception as error:
+    if type(error) == pymongo.errors.DuplicateKeyError:
+      db.update_one({"_id": gId}, {"list": content})
+
+def getEventList(gId):
+  collection = "event-content"
+  db = cli[DB_NAME][collection]
+  return db.find({"_id": gId}, {"_id": False})
+
+def setEventStatus(status, gId):
+  collection = "event"
+  db = cli[DB_NAME][collection]
+  try:
+    db.update_one({"_id": gId}, {"is_active": status}, True)
+  except Exception as error:
+    return error
+
+def getEventStatus(gId):
+  collection = "event"
+  db = cli[DB_NAME][collection]
+  try:
+    return db.find({"_id": gId}, {"_id": False})
+  except Exception as _error:
+    db.insert_one({"_id": gId, "is_active": False})
+    return False
+
 
 def getToken():
     return os.environ['TOKEN']
