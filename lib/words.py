80833863fffe08ea1df3a92c2e232b982e1bbc9f
GearFox98 <riveroluismanuel9@gmail.com>
2021-03-11 07:43:04 -0500

    Works

--- lib/words.py
+++ lib/words.py
@@ -17,8 +17,8 @@ Si quieres saber como hacer algunas cosas puedes usar /help
     }
 
 EVENT = {
-    'en' : '''Alright! Here\'s the new #CLOCKWORK_EVENT\n<i>Make sure you have <b>started me</b> on private, else you won't receive the notification</i>\n\n''',
-    'es' : '''¡Bien! Acá está el nuevo #CLOCKWORK_EVENT\n<<i>Asegúrate de <b>haberme iniciado</b> en privado, de otra forma no recibirás notificaciones</i>\n\n''',
+    'en' : '''Alright! Here\'s the new #CLOCKWORK_EVENT\n<i>Make sure you have started me on private, else you won't receive the notification</i>\n\n''',
+    'es' : '''¡Bien! Acá está el nuevo #CLOCKWORK_EVENT\n<i>Asegúrate de haberme iniciado en privado, de otra forma no recibirás notificaciones</i>\n\n''',
     'en_btn' : "I\'m in!",
     'es_btn' : "¡Cuenta conmigo!",
     'en_error' : "Sorry, there\'s an event running",
@@ -34,12 +34,12 @@ HELP = {
   'en' : '''Here\'s some help
 
 <b>/new_event <i>"event text"</i></b> - I'll start a new event, there's only allowed one event at time.
-<b>/raffle <i>"texto del sorteo" "número de participantes"</i></b> - I\'ll start a raffle.''',
+<b>/new_raffle <i>"texto del sorteo" "número de participantes"</i></b> - I\'ll start a raffle.''',
 #With the command /language you\'ll be able to change my language''',
   'es' : '''Acá un poco de ayuda
 
 <b>/new_event <i>"texto del evento"</i></b> - Iniciaré un evento, solo se permite un evento por grupo a la vez.
-<b>/raffle <i>"texto del sorteo" "número de participantes"</i></b> - Iniciaré un sorteo'''
+<b>/new_raffle <i>"texto del sorteo" "número de participantes"</i></b> - Iniciaré un sorteo'''
 #Con el comando /language podrás cambiar mi idioma'''
 }
 
