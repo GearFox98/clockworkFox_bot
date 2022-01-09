6eee93248cbc8468db3f9d6f8c7e8f097ce66d1d
Geartaro <riveroluismanuel9@gmail.com>
2022-01-09 02:24:28 -0500

    Refactored

--- bot.py
+++ bot.py
@@ -405,7 +405,7 @@ if __name__ == "__main__":
 
 
   updater.start_webhook(listen="0.0.0.0",
-                        port=PORT,
+                        port=8443,
                         url_path=TOKEN)
   updater.bot.set_webhook("https://clockworkfox-bot.herokuapp.com/" + TOKEN)
   
